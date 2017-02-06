ea536535f52ad61761a00862fe314777a48a668e
Victor Chernetsky <victor@amigocloud.com>
2017-02-06 13:28:59 -0800

    Replace icon, add support for AMIGOCLOUD_API_URL

--- amigo_api.py
+++ amigo_api.py
@@ -3,14 +3,20 @@ from amigocloud import AmigoCloud
 
 class AmigoAPI:
     def __init__(self):
-        token = os.environ['AMIGOCLOUD_API_KEY']
-        self.ac = AmigoCloud(token=token)
+        try:
+            self.token = os.environ['AMIGOCLOUD_API_KEY']
+            self.url = os.environ['AMIGOCLOUD_API_URL']
+            self.url = self.url[:-7]  # Clip '/api/v1' off
+        except:
+            self.url = 'https://www.amigocloud.com'
+
+        self.ac = AmigoCloud(token=self.token, base_url=self.url)
 
     def fetch_project_list(self):
-        resp = self.ac.get('https://www.amigocloud.com/api/v1/me/projects?summary')
+        resp = self.ac.get(self.url + '/api/v1/me/projects?summary')
         return resp['results']
 
     def fetch_dataset_list(self, project_id):
-        url = 'https://www.amigocloud.com/api/v1/users/0/projects/'+ project_id +'/datasets'
-        resp = self.ac.get(url)
+        dataset_url = self.url + '/api/v1/users/0/projects/' + project_id + '/datasets'
+        resp = self.ac.get(dataset_url)
         return resp['results']
